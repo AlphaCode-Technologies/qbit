92ca641237f1374e92463083bf3e2241f882a246
Dulan Sudasinghe <dulans@alphacodetechnologies.com>
2024-12-02 12:30:57 +0530

    Dev (#5)
    * chore(setup): branch naming (#2)
    
    Confirm
    
    * chore(scaffolding): component structure (#4)
    
    confirm

--- src/types/elements.d.ts
+++ src/types/elements.d.ts
@@ -4,4 +4,24 @@ declare namespace AlphaElements {
   };
 
   type Actions = Action[];
+
+  type InputNode = {
+    [key: string]: function | string;
+  };
+
+  type Inputs = InputNode[];
+
+  type OutputNode = {
+    [key: string]: function | string;
+  };
+
+  type Outputs = OutputNode[];
+
+  type Manifest = {
+    name: string;
+
+    actions: Actions;
+    inputs: Inputs;
+    outputs: Outputs;
+  };
 }
