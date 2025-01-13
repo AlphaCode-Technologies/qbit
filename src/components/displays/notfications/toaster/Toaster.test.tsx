4cd781e127c9659c40bde03645b64c33ff4e0dae
alphacode-rajinthank <rajinthank@alphacodetechnologies.com>
2025-01-13 13:31:04 +0530

    test: write test cases for duration

--- src/components/displays/notfications/toaster/Toaster.test.tsx
+++ src/components/displays/notfications/toaster/Toaster.test.tsx
@@ -1,18 +1,41 @@
-import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import Toaster from './Toaster';
 import { ToasterSkin } from '@skins/defaults';
 
 const DEFAULT_PROPERTIES = {
   open: true,
   position: 'top-center',
-  renderer: ToasterSkin,
   testId: 'test-toaster',
-  duration: 4000,
+  duration: 2000,
   autoClose: true,
 };
 
-const renderToaster = ({ props }: com.elem.Shell<AlphaElements.ToasterProperties, AlphaElements.ToasterActions>) =>
-  render(<Toaster properties={props} />);
+const renderToaster = ({ props }: com.elem.Shell<AlphaElements.ToasterProperties, AlphaElements.ToasterActions>) => {
+  render(
+    <Toaster
+      renderers={{ renderer: ToasterSkin, childRenderer: ToasterSkin }}
+      keyExtractor={(_, i: number) => `top-center-${i}`}
+      className="m-2 shadow-xl"
+      {...props}
+    >
+      <svg width="350" height="80" viewBox="0 0 350 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="583" height="80" rx="4" fill="white" />
+        <path d="M12 16V64" stroke="#8E8E93" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+        <path
+          d="M40 30C34.48 30 30 34.48 30 40C30 45.52 34.48 50 40 50C45.52 50 50 45.52 50 40C50 34.48 45.52 30 40 30Z"
+          fill="#8E8E93"
+        />
+        <path d="M37.8077 44L34 40.4L35.2692 39.2L37.8077 41.6L43.7308 36L45 37.2L37.8077 44Z" fill="white" />
+        <text x="65" y="35" fill="#000" fontSize="16" fontFamily="Arial" dominantBaseline="middle">
+          This is the title
+        </text>
+        <text x="65" y="50" fill="#000" fontSize="16" fontFamily="Arial" dominantBaseline="middle">
+          And this is some more information
+        </text>
+      </svg>
+    </Toaster>,
+  );
+};
 
 describe('Test for toaster elements', () => {
   afterEach(() => {
@@ -36,14 +59,8 @@ describe('Test for toaster elements', () => {
     const element = await screen.queryByTestId(DEFAULT_PROPERTIES.testId!);
     expect(element).toBeInTheDocument();
 
-    waitFor(
-      () => {
-        setTimeout(() => {
-          const ele = screen.queryByTestId(DEFAULT_PROPERTIES.testId!);
-          expect(ele).toBeInTheDocument();
-        }, 5000);
-      },
-      { timeout: 5000 },
-    );
+    await new Promise((r) => setTimeout(r, DEFAULT_PROPERTIES.duration + 100));
+    const ele = screen.queryByTestId(DEFAULT_PROPERTIES.testId!);
+    expect(ele).not.toBeInTheDocument();
   });
 });
