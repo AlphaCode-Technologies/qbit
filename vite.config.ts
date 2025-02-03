a5371528fc1be12f70a0d1325f688e54700c714f
Rajinthan Kanesalingam <rajinthank@alphacodetechnologies.com>
2025-02-03 15:02:39 +0530

    chore(build): added build setup
    * feat: creating modal element
    
    * test: add test cases for  modal element
    
    * docs: add story book for  modal element
    
    * feat: create toaster
    
    * feat: toaster auto hide
    
    * test: added test cases
    
    * feat: update toaster according to the new structure
    
    * test: write test cases for duration
    
    * docs: write story book
    
    * chore: folder structure updated
    
    * fix: child renderer issue
    
    * fix: issue resolved
    
    * docs: resolved the error in the documentation print
    
    * chore(build): added build setup
    
    * fix: removed unwanted line
    
    * fix: name added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,69 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import fs from 'fs';
+
+const mainPackageJson = JSON.parse(fs.readFileSync(resolve(__dirname, 'package.json'), 'utf-8'));
+
+const basePackageJson = {
+  name: mainPackageJson.name,
+  private: false,
+  version: mainPackageJson.version,
+  type: mainPackageJson.type,
+  author: mainPackageJson.author,
+};
+
+function createPackageJson(folder: string, urlPostFix: string) {
+  const folderPath = resolve(__dirname, `dist/${folder}`);
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+  }
+  fs.writeFileSync(
+    `${folderPath}/package.json`,
+    JSON.stringify({ ...basePackageJson, name: `${basePackageJson.name}/${urlPostFix}` }, null, 2),
+  );
+}
+
+function generatePackageJson() {
+  return {
+    name: 'generate-package-json',
+    closeBundle() {
+      createPackageJson('components', 'shell');
+      createPackageJson('skins', 'skins');
+    },
+  };
+}
+
+function findEntries(dir: string, base = '') {
+  const entries = {} as any;
+  fs.readdirSync(dir).forEach((file) => {
+    const fullPath = resolve(dir, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      Object.assign(entries, findEntries(fullPath, `${base}${file}/`));
+    } else if (
+      (file.endsWith('.jsx') || file.endsWith('.tsx') || file.endsWith('.ts')) &&
+      !file.includes('.test.') &&
+      !file.includes('.stories.') &&
+      !file.includes('.d.ts') &&
+      !file.includes('.manifest.') &&
+      file !== 'App.tsx' &&
+      file !== 'main.tsx'
+    ) {
+      const name = `${base}${file.replace(/\.(tsx|jsx|ts)$/, '')}`;
+      entries[name] = fullPath;
+    }
+  });
+  return entries;
+}
+
+const srcDir = './src';
+const entries = findEntries(srcDir);
+entries['styles'] = resolve(__dirname, 'src/index.css');
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), generatePackageJson()],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
@@ -17,7 +76,20 @@ export default defineConfig({
       '@fixtures': path.resolve(__dirname, 'src/fixtures'),
     },
   },
-
+  build: {
+    rollupOptions: {
+      input: entries,
+      output: {
+        dir: 'dist',
+        format: 'es',
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name][extname]',
+      },
+      treeshake: false,
+    },
+    outDir: 'dist',
+  },
   preview: {
     port: 3000,
     open: true,
