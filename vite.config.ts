8de39dbab70095dcbe14c92f13626f53a94f8b17
Rajinthan Kanesalingam <rajinthank@alphacodetechnologies.com>
2025-03-18 09:46:26 +0530

    fix: cdn build issue fixed (#88)
    * feat: creating modal element
    
    * test: add test cases for  modal element
    
    * docs: add story book for  modal element
    
    * feat: create toaster
    
    * feat: toaster auto hide
    
    * test: added test cases
    
    * feat: update toaster according to the new structure
    
    * test: write test cases for duration
    
    * docs: write story book
    
    * chore: folder structure updated
    
    * fix: child renderer issue
    
    * fix: issue resolved
    
    * docs: resolved the error in the documentation print
    
    * chore(build): added build setup
    
    * fix: removed unwanted line
    
    * fix: name added
    
    * build: change build folders
    
    * docs: added license file
    
    * docs: added readme
    
    * fix: build issue fix
    
    * fix: fixed package.json
    
    * fix: vite config for build
    
    * fix: vitest.config fixed for exporting elements
    
    * fix: remove unwanted files in build
    
    * fix: change properties.d.ts properties.ts
    
    * fix: change com d.ts file export
    
    * fix: version changed
    
    * fix: change path value
    
    * fix: deployment issue fixed
    
    * fix: update error version
    
    * fix: resolved issue in build
    
    * fix: build separate build
    
    * fix: build config
    
    * fix: config issue resolved
    
    * fix: version update
    
    * fix: build config issue
    
    * fix: version updated
    
    * fix: update config
    
    * fix: version updated
    
    * fix: issue resolved
    
    * fix: ordered all the imports
    
    * fix: cdn build issue fixed
    
    ---------
    
    Co-authored-by: alphacode-kirushnarajg <kirushnarajg@alphacodetechnologies.com>
    Co-authored-by: alphacode-abarnans <abarnans@alphacodetechnologies.com>

--- vite.config.ts
+++ vite.config.ts
@@ -64,24 +64,35 @@ export default defineConfig({
       entryRoot: './src',
     }),
   ],
+  // build: {
+  //   emptyOutDir: true,
+  //   lib: {
+  //     entry: path.resolve(__dirname, entryFile),
+  //     name: mainPackageJson.name,
+  //     formats: ['es', 'cjs', 'umd'],
+  //     fileName: (format) => `${mainPackageJson.name}.${format}.js`,
+  //   },
+  //   rollupOptions: {
+  //     external: ['react', 'react-dom', 'react/jsx-runtime'],
+  //     output: {
+  //       globals: {
+  //         react: 'React',
+  //         'react-dom': 'react-dom',
+  //         'react/jsx-runtime': 'react/jsx-runtime',
+  //       },
+  //       exports: 'named',
+  //     },
+  //   },
+  // },
+  define: {
+    'process.env': {}, // 👈 Fix "process is not defined" error
+  },
   build: {
-    emptyOutDir: true,
     lib: {
       entry: path.resolve(__dirname, entryFile),
       name: mainPackageJson.name,
       formats: ['es', 'cjs', 'umd'],
       fileName: (format) => `${mainPackageJson.name}.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'react-dom',
-          'react/jsx-runtime': 'react/jsx-runtime',
-        },
-        exports: 'named',
-      },
     },
   },
   preview: {
