a602944a4440a497f0ba04d49717c952e184d772
alphacode-dilshant <dilshant@alphacodetechnologies.com>
2025-04-04 15:32:09 +0530

    feat: create brand skin

--- src/skins/defaults/breadcrumb/Breadcrumb.skin.default.tsx
+++ src/skins/defaults/breadcrumb/Breadcrumb.skin.default.tsx
@@ -2,10 +2,10 @@ import { BreadcrumbProps } from '@components/index';
 import { com } from 'src/types/common';
 
 const BreadcrumbSkin: com.qbit.Skin<BreadcrumbProps> = (props: com.qbit.SkinProps<BreadcrumbProps>) => {
-  const { children } = props;
+  const { children, className, style } = props;
 
   return (
-    <nav aria-label="breadcrumb">
+    <nav aria-label="breadcrumb" className={className ?? ''} style={style}>
       <ol className="flex gap-2">{children}</ol>
     </nav>
   );
--- src/skins/defaults/breadcrumb/BreadcrumbItem.skin.default.tsx
+++ src/skins/defaults/breadcrumb/BreadcrumbItem.skin.default.tsx
@@ -1,26 +1,26 @@
-import { BreadcrumbItemProps } from '@components/index';
 import { com } from 'src/types/common';
+import { BreadcrumbItemProps } from '@components/index';
 
 const BreadcrumbItemSkin: com.qbit.Skin<BreadcrumbItemProps> = (props) => {
-  const { href, disabled, testId, value, active, onClick, index, childrenCount } = props;
+  const { href, disabled, testId, value, active, onClick, splitter, className, style } = props;
 
   return (
-    <div
-      onClick={(e: any) => onClick?.(e)}
-      className={`${disabled ? 'opacity-50 cursor-not-allowed pointer-events-none' : 'cursor-pointer'}`}
+    <li
+      onClick={onClick}
+      className={`${disabled ? 'opacity-50 cursor-not-allowed pointer-events-none' : 'cursor-pointer'} ${className ?? ''}`}
+      style={style}
       data-testid={testId}
     >
       {active ? (
         <span className="text-blue-500 font-medium"> {value} </span>
       ) : (
+        // TODO: Need to change as Link
         <a href={href} className="text-gray-500 hover:text-green-600">
           {value}
         </a>
       )}
-      {index !== undefined && childrenCount !== undefined && index < childrenCount - 1 && (
-        <span className="mx-2 text-gray-400">/</span>
-      )}
-    </div>
+      {splitter && <span className="mx-2 text-gray-400">{splitter}</span>}
+    </li>
   );
 };
 
