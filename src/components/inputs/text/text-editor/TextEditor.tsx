a592c226903592b9685ec97ebd04910cb4b0681e
alphacode-abarnans <abarnans@alphacodetechnologies.com>
2025-04-07 14:43:23 +0530

    feat: text editor

---
+++ src/components/inputs/text/text-editor/TextEditor.tsx
@@ -0,0 +1,52 @@
+import { BaseComponent } from '@components/containers';
+import { com } from 'src/types/common';
+import useTextEditor from './TextEditor.hook';
+import { TextEditorProps } from './properties';
+
+/**
+ * TextEditor component.
+ * @param props
+ * @returns JSX.Element
+ */
+const TextEditor: com.qbit.Shell<TextEditorProps> = (props: com.qbit.ShellProps<TextEditorProps>) => {
+  const { ...rest } = props;
+
+  const {
+    content,
+    fontFamily,
+    fontSize,
+    isBold,
+    isItalic,
+    isUnderline,
+    handleContentChange,
+    toggleBold,
+    toggleItalic,
+    toggleUnderline,
+    changeFontFamily,
+    changeFontSize,
+    fontOptions,
+    fontSizeOptions,
+  } = useTextEditor(rest);
+
+  return (
+    <BaseComponent
+      {...props}
+      content={content}
+      fontFamily={fontFamily}
+      fontSize={fontSize}
+      isBold={isBold}
+      isItalic={isItalic}
+      isUnderline={isUnderline}
+      handleContentChange={handleContentChange}
+      toggleBold={toggleBold}
+      toggleItalic={toggleItalic}
+      toggleUnderline={toggleUnderline}
+      changeFontFamily={changeFontFamily}
+      changeFontSize={changeFontSize}
+      fontOptions={fontOptions}
+      fontSizeOptions={fontSizeOptions}
+    />
+  );
+};
+
+export default TextEditor;
